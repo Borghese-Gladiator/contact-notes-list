4e7426073d30053c408ec0776e285c9695eaed44
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-10 20:06:14 -0500

    remove unused layout components

--- src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Navbar() {
-  return (
-    <footer>
-      <p>Author: Hege Refsnes</p>
-      <p><a href="mailto:hege@example.com">hege@example.com</a></p>
-    </footer>
-  )
-}
